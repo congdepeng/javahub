608fd72c9fd2318a53d05964408917b471833b9b
Cong Depeng <congdepeng@gmail.com>
2014-03-04 01:50:22 +0800

    字面量和原始类型的上下限

---
+++ lang/src/main/java/depeng/lang/primitive/define_init_not_constance/Wrapper.java
@@ -0,0 +1,10 @@
+package depeng.lang.primitive.define_init_not_constance;
+
+/**
+ * Created by depeng on 4/3/14.
+ */
+public class Wrapper {
+    int var;
+
+
+}
--- lang/src/main/java/depeng/lang/primitive/floatDemo.java
+++ lang/src/main/java/depeng/lang/primitive/floatDemo.java
@@ -7,7 +7,7 @@ public class floatDemo {
     public static void main(String[] args) {
         float f = 0;
 
-        f=
+
 
 
     }
