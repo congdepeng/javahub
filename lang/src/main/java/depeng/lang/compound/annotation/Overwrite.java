b5c0a55a0398b42f0d31e6e922facf9af5227ed9
Cong Depeng <congdepeng@gmail.com>
2014-03-11 01:15:33 +0800

    注解

---
+++ lang/src/main/java/depeng/lang/compound/annotation/Overwrite.java
@@ -0,0 +1,13 @@
+package depeng.lang.compound.annotation;
+
+/**
+ * Created by depeng on 11/3/14.
+ */
+public class Overwrite {
+
+    //java: method does not override or implement a method from a supertype
+//    @Override
+    public void oo() {
+        System.out.println("over");
+    }
+}
