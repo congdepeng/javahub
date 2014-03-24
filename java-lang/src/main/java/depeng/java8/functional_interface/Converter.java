3bf905958dcacb17fc0e4efeccf8867814e08dd0
Cong Depeng <congdepeng@gmail.com>
2014-03-24 11:59:41 +0800

    working place jdk 8 not support

--- java-lang/src/main/java/depeng/java8/functional_interface/Converter.java
+++ java-lang/src/main/java/depeng/java8/functional_interface/Converter.java
@@ -3,7 +3,7 @@ package depeng.java8.functional_interface;
 /**
  * Created by depeng on 23/3/14.
  */
-@FunctionalInterface
+//@FunctionalInterface
 public interface Converter<F, T> {
 
     T convert(F from);
--- java-lang/src/main/java/depeng/java8/functional_interface/Main.java
+++ java-lang/src/main/java/depeng/java8/functional_interface/Main.java
@@ -7,37 +7,37 @@ import java.util.HashMap;
  */
 public class Main {
     public static void main(String[] args) {
-        Converter converter_a_obj= new Converter() {
-            @Override
-            public Object convert(Object o) {
-                return Integer.valueOf(o.toString());
-            }
-        };
-
-
-
-        int fi =10;
-        Converter<String, Integer> converter_f_i = (from) -> Integer.valueOf(fi+from);
-        Integer convert = converter_f_i.convert("200");
-        System.out.println(convert);
-        //fi =300; have implicitly final int
-
-        Converter<String, Integer> converter_f_i_m_r = Integer::valueOf;
-
-
-        //method ref from object
-        SomeObject depeng = new SomeObject("depeng");
-
-        Converter<String, String> obj_converter = depeng::startsWith;
-        String abc = obj_converter.convert("abc");
-        System.out.println(abc);
-
-
-//        （Converter<String, Integer>    ((from) -> Integer.valueOf(from)))\
-
-
-        HashMap<Object, Object> objectObjectHashMap = new HashMap<>();
-//        objectObjectHashMap.computeIfAbsent()
+//        Converter converter_a_obj= new Converter() {
+//            @Override
+//            public Object convert(Object o) {
+//                return Integer.valueOf(o.toString());
+//            }
+//        };
+//
+//
+//
+//        int fi =10;
+//        Converter<String, Integer> converter_f_i = (from) -> Integer.valueOf(fi+from);
+//        Integer convert = converter_f_i.convert("200");
+//        System.out.println(convert);
+//        //fi =300; have implicitly final int
+//
+//        Converter<String, Integer> converter_f_i_m_r = Integer::valueOf;
+//
+//
+//        //method ref from object
+//        SomeObject depeng = new SomeObject("depeng");
+//
+//        Converter<String, String> obj_converter = depeng::startsWith;
+//        String abc = obj_converter.convert("abc");
+//        System.out.println(abc);
+//
+//
+////        （Converter<String, Integer>    ((from) -> Integer.valueOf(from)))\
+//
+//
+//        HashMap<Object, Object> objectObjectHashMap = new HashMap<>();
+////        objectObjectHashMap.computeIfAbsent()
 
     }
 }
