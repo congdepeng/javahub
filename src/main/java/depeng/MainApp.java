f41204e96af1e6b71b11b4bd50e599770319754d
depeng <congdepeng@gmail.com>
2015-02-11 15:19:29 +0800

    init gradle and test

---
+++ src/main/java/depeng/MainApp.java
@@ -0,0 +1,11 @@
+package depeng;
+
+public class MainApp {
+    public static void main(String[] args) {
+        System.out.println("Hello Gradle!");
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
