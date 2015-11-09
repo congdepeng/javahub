a26185ff0ab8e57932d26b3c9a366adfa25f2915
德鹏 <congdepeng@gmail.com>
2015-11-09 14:59:31 +0800

    big

---
+++ src/test/java/depeng/guava/BigDecimalTest.java
@@ -0,0 +1,12 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2015 All Rights Reserved.
+ */
+package depeng.guava;
+
+/**
+ * @author depeng.cdp
+ * @version $Id: BigDecimalTest.java, v 0.1 2015/10/24 depeng.cdp Exp $
+ */
+public class BigDecimalTest {
+}
