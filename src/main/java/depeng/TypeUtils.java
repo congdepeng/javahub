334f176296d66b5b6e67a931d447730b1c0493e6
Cong Depeng <congdepeng@gmail.com>
2016-07-15 17:03:22 +0800

    yes, remove gradle

---
+++ src/main/java/depeng/TypeUtils.java
@@ -0,0 +1,18 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2016 All Rights Reserved.
+ */
+package depeng;
+
+/**
+ * @author depeng.cdp
+ * @version $Id: TypeUtils.java, v 0.1 2016/2/25 depeng.cdp Exp $
+ */
+public class TypeUtils {
+
+    public static void main(String[] args) {
+        System.out.println(Integer.getInteger("888"));
+        System.out.println(Integer.parseInt("888"));
+    }
+
+}
